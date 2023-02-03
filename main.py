aeb8ca9ec5e41dea4d262b2f77bba553da123c74
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2023-02-03 19:46:01 +0000

    d

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 
 FILEPATH = input()
+
+with open(FILEPATH) as f:
+    RAWCORDS = f.read()
+CORDCOUNT = int()
+for line in open(FILEPATH):
+    print()
+    CORDCOUNT += 1
+print(RAWCORDS)
+print(CORDCOUNT)
+    
